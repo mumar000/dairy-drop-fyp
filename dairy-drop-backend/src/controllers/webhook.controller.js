f796571cd7e5721a32275cb719c9498de733fc35
Muhammad Annas Khan <annasking601@gmail.com>
2026-03-19 04:11:33 +0500

    fixes paid part 1

--- dairy-drop-backend/src/controllers/webhook.controller.js
+++ dairy-drop-backend/src/controllers/webhook.controller.js
@@ -1,6 +1,8 @@
 import { stripe } from "../utils/stripe.js";
 import { env } from "../config/env.js";
 import { Order } from "../models/order.model.js";
+import { Product } from "../models/product.model.js";
+import { User } from "../models/user.model.js";
 
 export const handleStripeWebhook = async (req, res) => {
   const sig = req.headers["stripe-signature"];
@@ -21,15 +23,41 @@ export const handleStripeWebhook = async (req, res) => {
     const orderId = session.metadata?.orderId;
 
     if (orderId) {
-      await Order.updateOne(
-        { _id: orderId },
-        {
-          $set: {
-            paymentStatus: "Paid",
-            stripePaymentIntentId: session.payment_intent,
-          },
-        },
-      );
+      const order = await Order.findById(orderId);
+
+      if (order && order.paymentStatus !== "Paid") {
+        order.paymentStatus = "Paid";
+        order.stripePaymentIntentId = session.payment_intent;
+        await order.save();
+
+        for (const item of order.items) {
+          await Product.updateOne(
+            { _id: item.product, inStock: { $gte: item.quantity } },
+            { $inc: { inStock: -item.quantity } },
+          );
+        }
+
+        const user = await User.findById(order.user);
+        if (user) {
+          const existingAddressIndex = user.addresses.findIndex(
+            (addr) =>
+              addr.line1 === order.address.line1 &&
+              addr.city === order.address.city &&
+              addr.postalCode === order.address.postalCode,
+          );
+
+          if (existingAddressIndex === -1) {
+            user.addresses.push({
+              ...order.address.toObject(),
+              label: order.address.name,
+              isDefault: user.addresses.length === 0,
+            });
+          }
+
+          user.cart = [];
+          await user.save();
+        }
+      }
     }
   }
 
